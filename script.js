9a9f8a7ed2557545bbd52a90781e2fbd0c125a94
Vikrant Pareek <vikrantpareek001@gmail.com>
2025-07-07 14:37:55 +0530

    added a feature

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ for (let faq of faqs) {
   faq.addEventListener("click", () => {
     const p = faq.getAttribute("p");
     faq.nextElementSibling.innerHTML = p;
+    let last = faq.childNodes[3];
     if (faq.nextElementSibling.style.display != "block") {
       faq.nextElementSibling.style.display = "block";
-    } else{
+      last.style.transform = "rotate(45deg)";
+    } else {
       faq.nextElementSibling.style.display = "none";
+      last.style.transform = "rotate(0deg)";
     }
   });
 }
-
